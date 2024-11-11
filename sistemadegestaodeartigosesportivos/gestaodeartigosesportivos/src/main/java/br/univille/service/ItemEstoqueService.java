cc3d3ba3206e6c9d8490df9c7c35002a49632236
Karolayne <99550771+karolaynedasilva@users.noreply.github.com>
2024-11-11 15:47:10 -0300

    Delete sistemadegestaodeartigosesportivos directory

--- sistemadegestaodeartigosesportivos/gestaodeartigosesportivos/src/main/java/br/univille/service/ItemEstoqueService.java
+++
@@ -1,8 +0,0 @@
-package br.univille.gestaodeartigosesportivos.entity;
-
-import br.univille.gestaodeartigosesportivos.entity.ItemEstoque;
-
-public interface ItemEstoqueService {
-    
-    ItemEstoque getResumo();
-}
